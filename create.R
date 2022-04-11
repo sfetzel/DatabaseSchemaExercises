7d5c88dad40694b5d5cf9e357781c0a39f7b06fc
Simon Fetzel <simonf93@univie.ac.at>
2022-04-11 09:47:11 +0200

    adapted exercises for Moodle

---
+++ create.R
@@ -0,0 +1,5 @@
+library(exams)
+
+exercises = c(rep("LeftSideReduction.Rnw", 2), rep("RightSideReduction.Rnw", 2), rep("KeyCandidates.Rnw", 2))
+
+exams2moodle(exercises, name="Functional dependencies exercises")
