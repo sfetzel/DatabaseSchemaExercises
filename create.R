246a38d5b4d3fd0007b94c764de2a687f262b0fd
Matthäus Nössing <noessing.matthaeus@protonmail.com>
2023-10-05 16:12:53 +0200

    Fix all false answers

--- create.R
+++ create.R
@@ -1,5 +1,18 @@
 library(exams)
 
-exercises = c(rep("LeftSideReduction.Rnw", 2), rep("RightSideReduction.Rnw", 2), rep("KeyCandidates.Rnw", 2))
+
+seed=2023
+set.seed(seed)
+exercises = c(rep("LeftSideReduction.Rnw", 2), rep("RightSideReduction.Rnw", 2))
+
+
 
 exams2moodle(exercises, name="Functional dependencies exercises")
+set.seed(seed)
+exams2moodle(rep("KeyCandidates.Rnw", 10), name="Key Kandidates")
+set.seed(seed)
+exams2html(exercises, name="Functional dependencies exercises")
+set.seed(seed)
+exams2html(rep("KeyCandidates.Rnw", 10), name="Key Kandidates")
+
+
